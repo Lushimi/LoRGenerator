32f66bde727e95dd504fb6354a18c61ec4ac8c5c
David Barsamian <spongeguy97@gmail.com>
2019-11-19 15:38:13 -0800

    - Set up structure - Started writing out stuff

--- src/main/java/com/studiogaffers/lorgenerator/LorgeneratorApplication.java
+++ src/main/java/com/studiogaffers/lorgenerator/LorgeneratorApplication.java
@@ -1,7 +1,12 @@
 package com.studiogaffers.lorgenerator;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class LorgeneratorApplication {
@@ -9,5 +14,6 @@ public class LorgeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(LorgeneratorApplication.class, args);
     }
+
 
 }
---
+++ src/main/java/com/studiogaffers/lorgenerator/util/DeckGenerator.java
@@ -0,0 +1,9 @@
+package com.studiogaffers.lorgenerator.util;
+
+public class DeckGenerator {
+
+    public static String generateDeck() {
+        return "DECK HERE";
+    }
+
+}
