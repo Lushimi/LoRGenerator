f78faf2a0340fde302689af849fa9cf5feae8276
David Barsamian <spongeguy97@gmail.com>
2019-11-18 23:28:38 -0800

    Initialized Java Spring project in root

---
+++ src/main/java/com/studiogaffers/lorgenerator/LorgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.studiogaffers.lorgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LorgeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LorgeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studiogaffers/lorgenerator/LorgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiogaffers.lorgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LorgeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
