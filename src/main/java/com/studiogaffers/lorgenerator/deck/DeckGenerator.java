974c6f7c771acab747547514f1b509b747f18111
David Barsamian <spongeguy97@gmail.com>
2019-11-21 17:56:10 -0800

    - changed other stuff

--- src/main/java/com/studiogaffers/lorgenerator/deck/DeckGenerator.java
+++ src/main/java/com/studiogaffers/lorgenerator/deck/DeckGenerator.java
@@ -7,13 +7,14 @@ import java.io.*;
 
 public class DeckGenerator {
 
-    public static String generateDeck() throws IOException {
-        Process p = Runtime.getRuntime().exec("python python/main.py");
-        System.out.println(p.isAlive());
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        System.out.println(stdInput.readLine());
+    public static String generateDeck() {
+        try {
+            Process p = Runtime.getRuntime().exec("python python/main.py");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.toString());
+        }
 
-        return stdInput.readLine();
     }
 
 }
